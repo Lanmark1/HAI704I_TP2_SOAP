8efa8f0cfd7fc550e9cc49d9bde9ac1fc7d282f0
yoann reyné <yoann.reyne@gmail.com>
2022-10-27 21:04:35 +0200

    add

---
+++ Yoann/SOAP/hotel.web.service/src/main/java/hotel/web/service/model/LocalDateAdapter.java
@@ -0,0 +1,18 @@
+package hotel.web.service.model;
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+    
+	public LocalDateAdapter() {};
+	
+	public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v);
+    }
+
+    public String marshal(LocalDate v) throws Exception {
+        return v.toString();
+    }
+}
+
--- Yoann/SOAP/hotel.web.service/src/main/java/hotel/web/service/publisher/main/HotelPublisher.java
+++ Yoann/SOAP/hotel.web.service/src/main/java/hotel/web/service/publisher/main/HotelPublisher.java
@@ -4,18 +4,21 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.ws.Endpoint;
 
 import hotel.web.service.model.Hotel;
 import hotel.web.service.model.Offre;
+import hotel.web.service.model.LocalDateAdapter;
 import hotel.web.service.services.HotelServiceConsultImpl;
 
+@XmlJavaTypeAdapter(value = LocalDateAdapter.class)
 public class HotelPublisher {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ArrayList<Hotel> hotels = getHotels();
 		
-		System.out.println(hotels);
+		System.out.println(hotels.get(0).getOffreById(1000).getDateDispo());
 		
 		Endpoint.publish("http://localhost:8080/hotelserviceconsult", new HotelServiceConsultImpl(hotels));
 		System.err.println("Server is ready");
@@ -24,16 +27,17 @@ public class HotelPublisher {
 	
 	}
 	
-	public static ArrayList<Hotel> getHotels(){
-		ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+	public static ArrayList<Hotel> getHotels() throws Exception{
 		
+		ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+		LocalDateAdapter date = new LocalDateAdapter();
 		ArrayList<Offre> offresH2 = new ArrayList<Offre>();
 		ArrayList<Offre> offresH1 = new ArrayList<Offre>();
 		
-		Offre of1h1 = new Offre(1000, 3, LocalDate.parse("2022-10-10"), 100 );
+		Offre of1h1 = new Offre(1000, 3, date.unmarshal("2022-10-10"), 100 );
 		offresH1.add(of1h1);
 		
-		Offre of1h2 = new Offre(1001, 4, LocalDate.parse("2022-10-10"), 110 );
+		Offre of1h2 = new Offre(1001, 4, date.unmarshal("2022-10-10"), 110 );
 		offresH2.add(of1h2);
 		
 	
